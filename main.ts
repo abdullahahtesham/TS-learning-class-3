ffb4721d09cab3b9d697c5b641ef497dd7acc1f9
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-03-25 10:55:01 +0500

    update it

---
+++ main.ts
@@ -0,0 +1,57 @@
+// const apple1 = 10
+// const apple2 = 6
+
+// console.log(apple1 + apple2)
+
+
+
+// const bana1 = 6
+// const bana2 = 9
+// const sub =bana1 - bana2
+// console.log(sub);
+// const mul1 =10
+// const mul2 =2
+// const mul =mul1*mul2
+// console.log(mul)
+// const div1 = 10
+// const div2 = 9
+// const div =div1 / div2
+// console.log(div)
+// const pencils = 21
+// const students = 5
+// const mod= pencils%students   //for remainder we use the mod
+// console.log(mod)
+// const pow1 = 5
+// const pow2 = 2
+// const power= pow1**pow2  // for remainder we use th power  operator **
+// console.log(power)
+
+// // // const firstName = "Ubaid"
+// // // const lastName = "ur rehman"
+// // // console.log (firstName+lastName)    //this is the concotion when we add the two string
+
+
+// // // const firstName = "Ubaid"
+// // // const lastName = "ur rehman"
+// // // console.log (firstName+" "+lastName)     // this is also a way to concotin two strings with a space in between them 
+// // const firstName =Abdullah 
+// // const lastName =Ahtesham
+// // console.log (firstName-lastName)     // this is not allowed because it's a string and you are trying to do math on strings so it will give an error
+// // two string will always added not sub, mul,div, or pow
+// // let n=5
+// // n = n+5
+// // console.log(n)
+// // Second easy method 
+// // let n=5
+// // n+=5
+// // console.log(n)
+// // console.log(10=10)
+// // console.log(10=="10")    //== this will not check the type only the value will check
+// // console.log(10=="10abc") 
+// // console.log(10==="10")   //===this will also check the type
+// console.log(10!=5 )      //! is not equa to  
+// console.log(5<2);
+// console.log(5<=5)
+// console.log(2<=5)  //if one condition id ture and  other false it will  true
+
+
